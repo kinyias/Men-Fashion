dfb26499527b3b69d060e4b01c08376060e9c68e
Thai Tin Khang <64598898+kinyias@users.noreply.github.com>
2025-04-25 13:36:18 +0700

    Update structure tiptap

--- src/app/simple/page.tsx
+++ src/app/simple/page.tsx
@@ -1,9 +1,9 @@
-"use client"
-import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor"
-import { useState } from "react";
+'use client';
+import { SimpleEditor } from '@/components/tiptap/tiptap-templates/simple/simple-editor';
+import { useState } from 'react';
 
 export default function Page() {
-  const [content, setContent] = useState("");
-  console.log("content", content);
-  return <SimpleEditor onChange={setContent} />
+  const [content, setContent] = useState('');
+  console.log('content', content);
+  return <SimpleEditor onChange={setContent} />;
 }
