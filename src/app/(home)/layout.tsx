4131c80f6d0053f57f1fa804fb5babccd8ce5307
Thai Tin Khang <64598898+kinyias@users.noreply.github.com>
2025-05-27 11:28:23 +0700

    Update tawk.to chat

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,7 +3,7 @@ import Footer from '@/components/layouts/Footer';
 import Header from '@/components/layouts/Header';
 import { ScrollToTop } from '@/components/layouts/ScrollToTop';
 import { AuthProvider } from '@/context/auth-provider';
-
+import Script from "next/script";
 export default function HomeLayout({
   children,
 }: {
@@ -19,6 +19,23 @@ export default function HomeLayout({
       <ScrollToTop />
       <Footer />
       </div>
+      <Script
+          id="tawk-to"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/68353c38b68def19071580bf/1is7v6g12';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `,
+          }}
+        />
     </AuthProvider>
     </> 
   );
